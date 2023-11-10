d72062624af737b428f9120b4ce922f1512c562e
Chris Steenkamp <cdsteenkamp@gmail.com>
2023-11-10 11:56:58 +0000

    Add PacManGame

--- src/main/java/com/carniware/learning/LearningApplication.java
+++ src/main/java/com/carniware/learning/LearningApplication.java
@@ -6,12 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.carniware.learning.game.GameRunner;
 import com.carniware.learning.game.GamingConsole;
 import com.carniware.learning.game.MarioGame;
+import com.carniware.learning.game.PacManGame;
 
 @SpringBootApplication
 public class LearningApplication {
 
 	public static void main(String[] args) {
-		GamingConsole game = new MarioGame();
+		GamingConsole game = new PacManGame();
 		GameRunner runner = new GameRunner(game);
 		runner.run();
 	}
---
+++ src/main/java/com/carniware/learning/game/PacManGame.java
@@ -0,0 +1,23 @@
+package com.carniware.learning.game;
+
+public class PacManGame implements GamingConsole {
+    public String name() {
+        return "PacManGame";
+    }
+
+    public void up() {
+        System.out.println("^");
+    }
+
+    public void down() {
+        System.out.println("v");
+    }
+
+    public void left() {
+        System.out.println("<");
+    }
+
+    public void right() {
+        System.out.println(">");
+    }
+}
