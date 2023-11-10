1a9567b32185368f759a777efccc582f99337563
Chris Steenkamp <cdsteenkamp@gmail.com>
2023-11-10 12:26:53 +0000

    Mark MarioGame as a component for spring

--- src/main/java/com/carniware/learning/game/MarioGame.java
+++ src/main/java/com/carniware/learning/game/MarioGame.java
@@ -1,5 +1,8 @@
 package com.carniware.learning.game;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MarioGame implements GamingConsole {
     public String name() {
         return "MarioGame";
