afc62a14ef11853de12ad4485ecae3f74a4aa153
Chris Steenkamp <cdsteenkamp@gmail.com>
2023-11-10 11:51:56 +0000

    Add SuperContraGame

---
+++ src/main/java/com/carniware/learning/game/SuperContraGame.java
@@ -0,0 +1,23 @@
+package com.carniware.learning.game;
+
+public class SuperContraGame implements GamingConsole {
+    public String name() {
+        return "SuperContraGame";
+    }
+
+    public void up() {
+        System.out.println("up");
+    }
+
+    public void down() {
+        System.out.println("down");
+    }
+
+    public void left() {
+        System.out.println("left");
+    }
+
+    public void right() {
+        System.out.println("right");
+    }
+}
