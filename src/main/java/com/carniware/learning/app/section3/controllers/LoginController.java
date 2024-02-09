1a3d9c4f3abe42df49e63c9a8b14fad8cb35f7ff
Chris Steenkamp <cdsteenkamp@gmail.com>
2024-02-09 13:27:59 +0000

    Add debug logging to login page.

--- src/main/java/com/carniware/learning/app/section3/controllers/LoginController.java
+++ src/main/java/com/carniware/learning/app/section3/controllers/LoginController.java
@@ -1,5 +1,7 @@
 package com.carniware.learning.app.section3.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping("/login")
     public String login(@RequestParam String name, ModelMap model) {
+        logger.debug("Name parameter was set to " + name);
         model.put("name", name);
         return "login";
     }
